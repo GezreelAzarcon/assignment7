0a4f7a93c4a10f83e90b82b3d10cccaeeed43ab7
GezreelAzarcon <gezreelwazarcon@gmail.com>
2021-11-28 00:02:53 +0800

    Program number 1 save folder

---
+++ prog1SaveFolder/prog1.py
@@ -0,0 +1,24 @@
+#Program 1:
+#Create a program that ask for a sentence as input.
+#Display the number of words, vowels and consonants in the input 
+#ex.
+#input: Bahala kayo dyan
+#output:
+#words: 3
+#vowels: 6
+#consonants: 8
+
+sen = input('Hey there my nigga: ')
+vow = 0
+cons = 0
+for i in sen:
+    if i == 'a' or i == 'i' or i == 'u' or i == 'e' or i == 'o' or i == 'A' or i == 'I' or i == 'U' or i == 'E' or i == 'O':
+        vow += 1
+    elif i == ' ':
+        continue
+    else:
+        cons += 1
+print(len(sen.split()))
+print(vow)
+print(cons)
+
